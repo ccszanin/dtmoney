a6837205d2a813e31f93028456996d9d1a5bb1de
Carla Cristine <carlacs.cristine@gmail.com>
2022-03-08 15:22:49 -0300

    button color

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -47,6 +47,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         type="button"
         onClick={() => { setType('deposit'); }}
         isActive={type == 'deposit'}
+        activeColor="green"
         >
 
           <img src={incomeImg} alt="Entrada" />
@@ -57,6 +58,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         type="button"
         onClick={() => { setType('withdraw'); }}
         isActive={type == 'withdraw'}
+        activeColor="red"
         >
 
           <img src={outcomeImg} alt="Saída" />
